976fac32df2158dae38266ac2125ce7124ac8f64
Gabriel Mota <100182257+oGabrielMota@users.noreply.github.com>
2022-11-17 12:12:20 -0300

    Adicionando todos os arquivos do projeto

---
+++ src/componentes/Cabeçalho/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./estilo.css";
+
+const Cabecalho = () => {
+  return (
+    <header className="cabecalho">
+      <div
+        className="imagem-logo"
+        role="img"
+        aria-label="Logo do Sorteador"
+      ></div>
+      <img
+        className="participante"
+        src="/imagens/participante.png"
+        alt="Participante com um presente na mão"
+      />
+    </header>
+  );
+};
+
+export default Cabecalho;
